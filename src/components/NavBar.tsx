ec0abc5f4aae4ea57c1a786dfcdd33d645f0b624
DavuKX <103606165+DavuKX@users.noreply.github.com>
2023-11-28 10:26:47 -0500

    create cart component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Link from "next/link";
-import { Icons } from './Icons';
+import {Icons} from './Icons';
 import NavItems from "@/components/NavItems";
+import {buttonVariants} from "@/components/ui/button";
+import Cart from "@/components/Cart";
 
 const NavBar = () => {
+    const user = null;
+
     return (
         <div className='bg-white sticky z-50 top-0 inset-x-0 h-16'>
             <header className='relative bg-white'>
@@ -14,11 +18,55 @@ const NavBar = () => {
                             {/*TODO: Mobile nav*/}
                             <div className='ml-4 flex lg:ml-0'>
                                 <Link href={'/'}>
-                                    <Icons.logo className='h-10 w-10' />
+                                    <Icons.logo className='h-10 w-10'/>
                                 </Link>
                             </div>
                             <div className='hidden z-50 lg:ml-8 lg:block lg:self-stretch'>
-                                <NavItems />
+                                <NavItems/>
+                            </div>
+
+                            <div className='ml-auto flex items-center'>
+                                <div className='hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6'>
+                                    {user ? null
+                                        : <Link
+                                            href='/sign-in'
+                                            className={buttonVariants({variant: 'ghost'})}
+                                        >
+                                            Sign in
+                                        </Link>
+                                    }
+
+                                    {user ? null
+                                        : <span className='h-6 w-px bg-gray-200' aria-hidden={'true'}></span>
+                                    }
+
+                                    {user ? <p></p>
+                                        : <Link
+                                            href='/sign-up'
+                                            className={buttonVariants({variant: 'ghost'})}
+                                        >
+                                            Create account
+                                        </Link>
+                                    }
+
+                                    {user ?
+                                        <span className='h-6 w-px bg-gray-200' aria-hidden='true'></span>
+                                        : null
+                                    }
+
+                                    {user
+                                        ? null
+                                        : (
+                                            <div className='flex lg:ml-6'>
+                                                <span className='h-6 w-px bg-gray-200' aria-hidden='true'></span>
+                                            </div>
+                                        )
+                                    }
+
+                                    <div className='ml-4 flow-root lg:ml-6'>
+                                        <Cart />
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
