76faffaa14dc50f3a8e457c08a5a1d4d3e1edd81
DavuKX <103606165+DavuKX@users.noreply.github.com>
2023-11-30 18:01:42 -0500

    finish authentication flow

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -12,10 +12,21 @@ import {zodResolver} from "@hookform/resolvers/zod";
 import {AuthCredentialsValidator, TAuthCredentialsValidator} from "@/lib/validators/account-credentials-validator";
 import {trpc} from "@/trpc/client";
 import {toast} from "sonner";
-import {ZodError} from "zod";
-import {useRouter} from "next/navigation";
+import {useRouter, useSearchParams} from "next/navigation";
 
 const Page = () => {
+    const searchParams = useSearchParams();
+    const router = useRouter();
+    const isSeller = searchParams.get('as') === 'seller';
+    const origin = searchParams.get('origin');
+    const continueAsSeller = () => {
+        router.push('?as=seller')
+    }
+
+    const continueAsBuyer = () => {
+        router.replace('/sign-in', undefined)
+    }
+
     const {
         register,
         handleSubmit,
@@ -27,32 +38,33 @@ const Page = () => {
         resolver: zodResolver(AuthCredentialsValidator),
     });
 
-    const router = useRouter();
+    const {mutate: signIn, isLoading} = trpc.auth.signIn.useMutation({
+        onSuccess: () => {
+            toast.success('Signed in successfully!');
+            router.refresh();
 
-    const {mutate, isLoading} = trpc.auth.createPayloadUser.useMutation({
-        onError: (error) => {
-            if (error.data?.code === 'CONFLICT') {
-                toast.error('This email is already in use. Sign in instead');
-
+            if (origin) {
+                router.push(`/${origin}`);
                 return;
             }
 
-            if (error instanceof ZodError) {
-                toast.error(error.issues[0].message);
-
+            if (isSeller) {
+                router.push('/sell');
                 return;
             }
 
-            toast.error('Something went wrong. Please try again later.');
+            router.push('/');
         },
-        onSuccess: ({sentToEmail}) => {
-            toast.success(`We sent an email to ${sentToEmail}. Please check your inbox to verify your email address.`);
-            router.push('/verify-email?to=' + sentToEmail);
+        onError: (error) => {
+            if (error.data?.code === 'UNAUTHORIZED') {
+                toast.error('Invalid credentials!');
+                return;
+            }
         }
     });
 
     const onSubmit = ({email, password}: TAuthCredentialsValidator) => {
-        mutate({email, password});
+        signIn({email, password});
     }
 
     return (
@@ -62,13 +74,13 @@ const Page = () => {
                     <div className='flex-col flex items-center space-y-2 text-center'>
                         <Icons.logo className='w-20 h-20'/>
                         <h1 className='text-2xl font-bold'>
-                            Create an account
+                            Sign in to your {isSeller ? 'seller' : ''} account
                         </h1>
-                        <Link href='/sign-in' className={buttonVariants({
+                        <Link href='/sign-up' className={buttonVariants({
                             variant: 'link',
                             className: 'gap-1.5'
                         })}>
-                            Already have an account? Sign in
+                            Don&apos;t have an account?
                             <ArrowRight className='w-4 h-4'/>
                         </Link>
                     </div>
@@ -102,9 +114,34 @@ const Page = () => {
                                         <p className='text-sm text-red-500'>{errors.password?.message}</p>
                                     )}
                                 </div>
-                                <Button>Sign up</Button>
+                                <Button>Sign in</Button>
                             </div>
                         </form>
+                        <div className='relative' aria-hidden='true'>
+                            <div className='absolute inset-0 flex items-center'>
+                                <span className='w-full border-t'/>
+                            </div>
+                            <div className='relative flex justify-center text-xs uppercase'>
+                                <span className='bg-background px-2 text-muted-foreground'>
+                                    or
+                                </span>
+                            </div>
+                        </div>
+                        {isSeller ? (
+                            <Button
+                                onClick={continueAsBuyer}
+                                variant='secondary'
+                                disabled={isLoading}
+                            >
+                                Continue as customer
+                            </Button>
+                        ) : (
+                            <Button
+                                onClick={continueAsSeller}
+                                variant='secondary'
+                                disabled={isLoading}
+                            >Continue as seller</Button>
+                        )}
                     </div>
                 </div>
             </div>
