a376b37a615486ab2b97e0bc35c5aefd5bf53f64
DavuKX <103606165+DavuKX@users.noreply.github.com>
2023-12-12 10:55:46 -0500

    create user checkout session

---
+++ src/trpc/payment-router.ts
@@ -0,0 +1,78 @@
+import {privateProcedure, router} from "./trpc";
+import {z} from "zod";
+import {TRPCError} from "@trpc/server";
+import {getPayloadClient} from "../get-payload";
+import {stripe} from "../lib/stripe";
+import Stripe from "stripe";
+
+export const paymentRouter = router({
+    createSession: privateProcedure
+        .input(z.object({productIds: z.array(z.string())}))
+        .mutation(async ({ctx, input}) => {
+            const {user} = ctx
+            let {productIds} = input
+
+            if (productIds.length === 0) {
+                throw new TRPCError({code: 'BAD_REQUEST'})
+            }
+
+            const payload = await getPayloadClient()
+
+            const {docs: products} = await payload.find({
+                collection: 'products',
+                where: {
+                    id: {
+                        in: productIds,
+                    },
+                },
+            })
+
+            const filterProducts = products.filter((product) => Boolean(product.priceId))
+
+            const order = await payload.create({
+                collection: 'orders',
+                data: {
+                    _isPaid: false,
+                    products: filterProducts.map((product) => product.id),
+                    user: user.id
+                }
+            })
+
+            const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = []
+
+            filterProducts.forEach((product) => {
+                line_items.push({
+                    price: product.priceId!,
+                    quantity: 1,
+                })
+            })
+
+            line_items.push({
+                price: "price_1OMWp5JFLj8hC6n8WzpevDw2",
+                quantity: 1,
+                adjustable_quantity: {
+                    enabled: false,
+                }
+            })
+
+            try {
+                const stripeSession = await stripe.checkout.sessions.create({
+                    success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId${order.id}`,
+                    cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/cart`,
+                    payment_method_types: ["card"],
+                    mode: "payment",
+                    metadata: {
+                        userId: user.id,
+                        orderId: order.id
+                    },
+                    line_items
+                })
+
+                return {url: stripeSession.url}
+            } catch (e) {
+                console.error(e)
+
+                return {url: null}
+            }
+        })
+})
